1ebd5f3c87dfb030837a033b7f0e39503d6e1006
V <leCarreacute@gmail.com>
2026-02-24 17:09:18 +0200

    add editing page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,34 @@
-const { createProduct, fetchAll } = require("../models/product");
+const {
+  createProduct,
+  fetchAll,
+  findProductById,
+} = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", { pageTitle: "Add product" });
+  res.render("admin/add-product", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+    editing: "false",
+  });
+};
+
+exports.getEditProduct = async (req, res, next) => {
+  const editMode = req.query.edit === "true";
+  const prodId = req.params.productId;
+  const product = await findProductById(prodId);
+  console.log(product);
+
+  // replace to error page later
+  if (!product) {
+    return res.redirect("/");
+  }
+
+  res.render("admin/edit-product", {
+    pageTitle: "Add product",
+    path: "/admin/edit-product",
+    product: product,
+    editing: editMode,
+  });
 };
 
 exports.getProducts = async (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const adminController = require("../controllers/admin");
 
 router.get("/add-product", adminController.getAddProduct);
 router.get("/products", adminController.getProducts);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
 router.post("/add-product", adminController.postAddProduct);
 
